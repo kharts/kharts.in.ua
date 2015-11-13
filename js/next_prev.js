0b10c4f04a00adf3615335e4b3eac7abb735ef7b
kharts <kharts@ukr.net>
2015-11-13 00:33:49 -0800

    Add title of previous post

--- js/next_prev.js
+++ js/next_prev.js
@@ -40,9 +40,8 @@
             }
             return res;
         }
-        var next_link = document.getElementById("next_link");
-        var prev_link = document.getElementById("prev_link");
         var url_base = "http://" + document.domain + "/api/read/json?callback=?&id="
+        var next_link = document.getElementById("next_link");
         if (next_link != null) {
             var id = getIdFromUrl(next_link.href);
             if (id != "") {
@@ -54,6 +53,21 @@
                     })
                 var next_title = document.getElementById("next_title");
                 next_title.innerHTML = title;
+                })
+            }
+        }
+        var prev_link = document.getElementById("prev_link");
+        if (prev_link != null) {
+            var id = getIdFromUrl(prev_link.href);
+            if (id != "") {
+                var url = url_base + id;
+                $.getJSON(url, function(data) {
+                    title = "";
+                    $(data.posts).each(function(i, post) {
+                        title = getTitle(post);
+                    })
+                var prev_title = document.getElementById("prev_title");
+                prev_title.innerHTML = title;
                 })
             }
         }
