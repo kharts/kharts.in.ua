037d5721aebe57c136ffbd19a4df55a948d31ce0
kharts <kharts@ukr.net>
2015-11-13 00:28:47 -0800

    Fix errors

--- js/next_prev.js
+++ js/next_prev.js
@@ -2,7 +2,7 @@
 (function () {
     $(document).ready(function() {
         function getTitle(post) {
-            title = "";
+            var title = "";
             switch(post.type) {
                 case "regular":
                     title = post['regular-title'];
@@ -33,26 +33,26 @@
             return title;
         }
         function getIdFromUrl(url) {
-            res = "";
-            slash_pos = url.lastIndexOf("/");
+            var res = "";
+            var slash_pos = url.lastIndexOf("/");
             if (slash_pos != -1) {
                 res = url.slice(slash_pos, url.lenght - 1);
             }
             return res;
         }
-        next_link = document.getElementById("next_link");
-        prev_link = document.getElementById("prev_link");
-        url_base = "http://" + document.domain + "/api/read/json?callback=?&id="
+        var next_link = document.getElementById("next_link");
+        var prev_link = document.getElementById("prev_link");
+        var url_base = "http://" + document.domain + "/api/read/json?callback=?&id="
         if (next_link != null) {
-            id = getIdFromUrl(next_link.Href);
+            var id = getIdFromUrl(next_link.href);
             if (id != "") {
-                url = url_base + id;
+                var url = url_base + id;
                 $.getJSON(url, function(data) {
                     title = "";
                     $(data.posts).each(function(i, post) {
                         title = getTitle(post);
                     })
-                next_title = document.getElementById("next_title");
+                var next_title = document.getElementById("next_title");
                 next_title.innerHTML = title;
                 })
             }
