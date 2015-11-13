ebc6458fb85f5965c315986c0a675f369116ff22
kharts <kharts@ukr.net>
2015-11-13 00:07:29 -0800

    Add getIdFromUrl() function

--- js/next_prev.js
+++ js/next_prev.js
@@ -32,19 +32,30 @@
             }
             return title;
         }
+        function getIdFromUrl(url) {
+            res = "";
+            slash_pos = url.lastIndexOf("/");
+            if (slash_pos != -1) {
+                res = url.slice(slash_pos, url.lenght - 1);
+            }
+            return res;
+        }
         next_link = document.getElementById("next_link");
         prev_link = document.getElementById("prev_link");
         url_base = "http://" + document.domain + "/api/read/json?callback=?&id="
         if (next_link != null) {
-            url = url_base + next_link.Href;
-            $.getJSON(url, function(data) {
-                title = "";
-                $(data.posts).each(function(i, post) {
-                    title = getTitle(post);
-                })
+            id = getIdFromUrl(next_link.Href);
+            if (id != "") {
+                url = url_base + id;
+                $.getJSON(url, function(data) {
+                    title = "";
+                    $(data.posts).each(function(i, post) {
+                        title = getTitle(post);
+                    })
                 next_title = document.getElementById("next_title");
                 next_title.innerHTML = title;
-            })
+                })
+            }
         }
     })
 }) ();
