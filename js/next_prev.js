ed07be0b49854d7d8d0e5ce42b87cdb6a897fdd7
kharts <kharts@ukr.net>
2015-11-13 17:50:30 -0800

    Add fillTitle() function

--- js/next_prev.js
+++ js/next_prev.js
@@ -1,36 +1,28 @@
 // functions for displaying next and previous posts title
 (function () {
     $(document).ready(function() {
-        var url_base = "http://" + document.domain + "/api/read/json?callback=?&id=";
-        var next_link = document.getElementById("next_link");
-        if (next_link != null) {
-            var id = getIdFromUrl(next_link.href);
-            if (id != "") {
-                var url = url_base + id;
+        function fillTitle(url, title_id) {
+            var post_id = getIdFromUrl(url);
+            if (post_id != "") {
+                var url = url_base + post_id;
                 $.getJSON(url, function(data) {
                     title = "";
                     $(data.posts).each(function(i, post) {
                         title = getTitle(post);
                     })
-                var next_title = document.getElementById("next_title");
-                next_title.innerHTML = title;
+                var title_element = document.getElementById(title_id);
+                title_element.innerHTML = title;
                 })
             }
         }
+        var url_base = "http://" + document.domain + "/api/read/json?callback=?&id=";
+        var next_link = document.getElementById("next_link");
+        if (next_link != null) {
+            fillTitle(next_link.href, "next_title");
+        }
         var prev_link = document.getElementById("prev_link");
         if (prev_link != null) {
-            var id = getIdFromUrl(prev_link.href);
-            if (id != "") {
-                var url = url_base + id;
-                $.getJSON(url, function(data) {
-                    title = "";
-                    $(data.posts).each(function(i, post) {
-                        title = getTitle(post);
-                    })
-                var prev_title = document.getElementById("prev_title");
-                prev_title.innerHTML = title;
-                })
-            }
+            fillTitle(prev_link.href, "prev_title");
         }
     })
 }) ();
